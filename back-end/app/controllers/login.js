a4d68c1166e038f3b98b55299e05710ef7daf856
unknown <hrsoumya05@gmail.com>
2021-01-15 20:24:15 +0530

    Modified login

--- back-end/app/controllers/login.js
+++ back-end/app/controllers/login.js
@@ -2,13 +2,20 @@ const express = require("express");
 const router = express.Router();
 const { con } = require("../../config/db");
 
-router.get("/", (req, res) => {
+router.post("/", (req, res) => {
   let { uName, password } = req.body;
-  let findQuery = `SELECT * from users where first_name=${uName}`;
+  let findQuery = `SELECT * from users where first_name='${uName}'`;
   con.query(findQuery, function (err, result) {
-    if (err) res.send({ message: "Login failed", result: err });
-    console.log("user fetched::", result);
-    res.send({ message: "logged in successfully", result: result });
+    if (err) {
+      res.send({ message: "Login failed", data: err, status: "fail" });
+    } else {
+      console.log("user fetched::", result);
+      res.send({
+        message: "logged in successfully",
+        data: result,
+        status: "success",
+      });
+    }
   });
 });
 
