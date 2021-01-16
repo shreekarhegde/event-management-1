69ff1fe60875abded41b6ce678aab8e8fc87a588
unknown <hrsoumya05@gmail.com>
2021-01-16 19:52:11 +0530

    Added admin controller

---
+++ back-end/app/controllers/adminController.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+const { con } = require("../../config/db");
+
+router.get("/", (req, res) => {
+  let findQuery = `SELECT * FROM events 
+  join venue on events.venueID = venue.venueID
+  inner join payment on payment.userID = events.userID and payment.eventID = events.eventID`;
+  con.query(findQuery, function (err, result) {
+    if (err) {
+      res.send({
+        message: "Something went wrong. Could not fetch events",
+        data: err,
+        status: "fail",
+      });
+    } else {
+      console.log("admin events fetched::", result);
+      res.send({
+        message: "All events fetched!",
+        data: result,
+        status: "success",
+      });
+    }
+  });
+});
+
+module.exports = {
+  adminController: router,
+};
--- back-end/config/routes.js
+++ back-end/config/routes.js
@@ -5,12 +5,14 @@ const { loginController } = require("../app/controllers/login");
 const { paymentsController } = require("../app/controllers/paymentController");
 const { usersController } = require("../app/controllers/usersController");
 const { venueController } = require("../app/controllers/venueController");
+const { adminController } = require("../app/controllers/adminController");
 
 router.use("/users", usersController);
 router.use("/events", eventsController);
 router.use("/venue", venueController);
 router.use("/payment", paymentsController);
 router.use("/login", loginController);
+router.use("/admin", adminController);
 
 module.exports = {
   routes: router,
